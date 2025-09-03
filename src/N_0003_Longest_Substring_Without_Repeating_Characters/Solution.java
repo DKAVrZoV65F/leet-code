6736cba32bd259ce5486e714b52c699044940d5d
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2025-09-03 19:37:36 +0300

    feat: add solution for Longest Substring Without Repeating Characters problem (№3)

---
+++ src/N_0003_Longest_Substring_Without_Repeating_Characters/Solution.java
@@ -0,0 +1,34 @@
+package N_0003_Longest_Substring_Without_Repeating_Characters;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int result = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+
+            while (set.contains(ch)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(ch);
+            int length = right - left + 1;
+            result = Math.max(result, length);
+            System.out.println(set);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "pwwkew";
+        int result = lengthOfLongestSubstring(s);
+        System.out.print(result);
+    }
+}
