478cf3c58b69a5118879db0c345f0d120565ed32
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2025-09-03 21:18:24 +0300

    feat: add solution for Roman to Integer problem (№13)

---
+++ src/N_0013_roman_to_integer/Solution.java
@@ -0,0 +1,38 @@
+package N_0013_roman_to_integer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        char[] chars = s.toCharArray();
+        int result = 0;
+        int i, j;
+        for (i = 0, j = 1; j < chars.length; i++, j++) {
+            if (map.get(chars[i]) >= map.get(chars[j])) {
+                result += map.get(chars[i]);
+            } else {
+                result -= map.get(chars[i]);
+            }
+        }
+
+        result += map.get(chars[i]);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String text = "MI";
+        int result = romanToInt(text);
+        System.out.print(result);
+    }
+}
