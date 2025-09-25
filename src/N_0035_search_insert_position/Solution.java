bd33b6e56c8cd42d6eb03c5c66d2d31d7fb489db
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2025-09-25 19:13:47 +0300

    feat: add solution for Search Insert Position problem (№35)

---
+++ src/N_0035_search_insert_position/Solution.java
@@ -0,0 +1,28 @@
+package N_0035_search_insert_position;
+
+public class Solution {
+
+    public static int searchInsert(int[] nums, int target) {
+        int mid;
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        int result = searchInsert(nums, target);
+        System.out.print(result);
+    }
+}
