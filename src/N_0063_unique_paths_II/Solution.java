b9a8ff2fcd334bd7cd4ad2350d87c9e03bbc6eb3
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2025-09-27 00:48:53 +0300

    feat: add solution for Unique Paths II problem (№63)

---
+++ src/N_0063_unique_paths_II/Solution.java
@@ -0,0 +1,43 @@
+package N_0063_unique_paths_II;
+
+public class Solution {
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                break;
+            } else {
+                dp[i][0] = 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                break;
+            } else {
+                dp[0][i] = 1;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int[][] obstacleGrid = {{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
+        int result = uniquePathsWithObstacles(obstacleGrid);
+        System.out.print(result);
+    }
+}
